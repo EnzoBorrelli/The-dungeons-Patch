a76ad5da912010013de651430147373a2d9e3e0e
Enzo Borrelli <johndoe@example.comenzoborrellidev@gmail.com>
2023-01-11 15:09:00 -0300

    first upload

---
+++ src/main/java/com/EnderDev/thedungeonspatch/TheDungeonsPatch.java
@@ -0,0 +1,41 @@
+package com.EnderDev.thedungeonspatch;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(TheDungeonsPatch.MOD_ID)
+public class TheDungeonsPatch
+{
+
+    public static final String MOD_ID = "thedungeonspatch";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TheDungeonsPatch() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
